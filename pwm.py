9e8329f4b8e4fc96fd292c337e3af6570fb02455
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-07-30 13:39:47 -0400

    Configuration to make GPIO pins transmit PWM signal added

--- pwm.py
+++ pwm.py
@@ -4,27 +4,33 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 # Motor #1
+# PWM Pin 3
+GPIO.setup(3, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 
 # Motor #2
+# PWM Pin 5
+GPIO.setup(5, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
 # Motor # 1
+# Creating a PWM object
+myPWM = GPIO.PWM(3, 100)
+myPWM.ChangeDutyCycle(50)
+# Pin 11 and 12 allow us to control the direction of the motor
 GPIO.output(12, GPIO.LOW)
 GPIO.output(11, GPIO.HIGH)
 
 # Motor # 2
+myPWM = GPIO.PWM(5, 100)
+myPWM.ChangeDutyCycle(50)
 GPIO.output(15, GPIO.HIGH)
 GPIO.output(13, GPIO.LOW)
 
 # Wait 2.5 seconds
 time.sleep(2.5)
 
-# Reset all the GPIO pins by setting them to LOW
-GPIO.output(11, GPIO.LOW)
-GPIO.output(12, GPIO.LOW)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
-
+# Cleaning up the GPIO pin for next user
+GPIO.cleanup()
