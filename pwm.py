5e88c645cfd4d266e2a622bd1c77a7f6bb453eea
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-08-16 16:40:35 -0400

    Backward function added

--- pwm.py
+++ pwm.py
@@ -19,7 +19,7 @@ GPIO.setup(15, GPIO.OUT)
 
 def main():
     forward()
-    forward()
+    backward()
  # Reset all the GPIO pins by setting them to LOW
 
 
@@ -54,30 +54,21 @@ def forward():
     # Wait 5 seconds
     time.sleep(5)
 
-    reset()
 
-
-def right():
-    # Motor # 1
-    # Creating a PWM object
+def backward():
     myPWM = GPIO.PWM(3, 100)
     myPWM.start(50)
 
-    # Pin 11 and 12 allow us to control the direction of the motor
-    GPIO.output(12, GPIO.LOW)
-    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(11, GPIO.LOW)
 
-    # Motor # 2
     myPWM2 = GPIO.PWM(5, 100)
     myPWM2.start(50)
 
-    GPIO.output(15, GPIO.HIGH)
-    GPIO.output(13, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
 
-    # Wait 2.5 seconds
     time.sleep(0.5)
-
-    reset()
 
 
 main()
