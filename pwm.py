b0282e118dee81ef1a90b6ca952e3c791f32c990
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-08-16 16:36:08 -0400

    test

--- pwm.py
+++ pwm.py
@@ -5,11 +5,13 @@ GPIO.setmode(GPIO.BOARD)
 # Motor #1
 # PWM Pin 3
 GPIO.setup(3, GPIO.OUT)
+# Pin 11 and 12 allow us to control the direction of the Motor 1
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 
 # Motor #2
 # PWM Pin 5
+# Pin 13 and 15 allow us to control the direction of the Motor 1
 GPIO.setup(5, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
@@ -17,10 +19,13 @@ GPIO.setup(15, GPIO.OUT)
 
 def main():
     forward()
+    forward()
+
+ # Reset all the GPIO pins by setting them to LOW
 
 
 def reset():
-    # Reset all the GPIO pins by setting them to LOW
+
     GPIO.output(11, GPIO.LOW)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(13, GPIO.LOW)
@@ -29,6 +34,8 @@ def reset():
     # Cleaning up the GPIO pin for next user
     GPIO.cleanup()
 
+# Both motors are rotating forward
+
 
 def forward():
     # Motor # 1
@@ -36,7 +43,6 @@ def forward():
     myPWM = GPIO.PWM(3, 100)
     myPWM.start(50)
 
-    # Pin 11 and 12 allow us to control the direction of the motor
     GPIO.output(11, GPIO.HIGH)
     GPIO.output(12, GPIO.LOW)
 
@@ -46,7 +52,7 @@ def forward():
     GPIO.output(13, GPIO.LOW)
     GPIO.output(15, GPIO.HIGH)
 
-    # Wait 2.5 seconds
+    # Wait 5 seconds
     time.sleep(5)
 
     reset()
