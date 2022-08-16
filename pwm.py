1a56ce8029fd20b24619a95fcb1ab182bfc3c800
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-08-16 16:20:29 -0400

    Right function added

--- pwm.py
+++ pwm.py
@@ -15,11 +15,48 @@ GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
 
+def main():
+    forward()
+
+
+def reset():
+    # Reset all the GPIO pins by setting them to LOW
+    GPIO.output(11, GPIO.LOW)
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW)
+
+    # Cleaning up the GPIO pin for next user
+    GPIO.cleanup()
+
+
 def forward():
     # Motor # 1
     # Creating a PWM object
     myPWM = GPIO.PWM(3, 100)
-    myPWM.start(99)
+    myPWM.start(50)
+
+    # Pin 11 and 12 allow us to control the direction of the motor
+    GPIO.output(11, GPIO.HIGH)
+    GPIO.output(12, GPIO.LOW)
+
+    # Motor # 2
+    myPWM2 = GPIO.PWM(5, 100)
+    myPWM2.start(50)
+    GPIO.output(13, GPIO.LOW)
+    GPIO.output(15, GPIO.HIGH)
+
+    # Wait 2.5 seconds
+    time.sleep(5)
+
+    reset()
+
+
+def right():
+    # Motor # 1
+    # Creating a PWM object
+    myPWM = GPIO.PWM(3, 100)
+    myPWM.start(50)
 
     # Pin 11 and 12 allow us to control the direction of the motor
     GPIO.output(12, GPIO.LOW)
@@ -27,20 +64,15 @@ def forward():
 
     # Motor # 2
     myPWM2 = GPIO.PWM(5, 100)
-    myPWM2.start(99)
+    myPWM2.start(50)
 
     GPIO.output(15, GPIO.HIGH)
     GPIO.output(13, GPIO.LOW)
 
     # Wait 2.5 seconds
-    time.sleep(5)
+    time.sleep(0.5)
+
+    reset()
 
 
-# Reset all the GPIO pins by setting them to LOW
-GPIO.output(11, GPIO.LOW)
-GPIO.output(12, GPIO.LOW)
-GPIO.output(13, GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
-
-# Cleaning up the GPIO pin for next user
-GPIO.cleanup()
+main()
