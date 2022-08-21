0e5a25ceeef4ed657b671b60102e83def09b7a90
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-08-21 09:38:19 -0400

    Code refactored

--- distance.py
+++ distance.py
@@ -11,23 +11,28 @@ GPIO.setup(echoPin, GPIO.IN)
 try:
     while True:
         GPIO.output(trigPin, 1)
-        # Sleep for 2 micro seconds
+        # The Ultrasonic Transmitter in the Sensor generates a 40 KHz Ultrasound. In order to send the 40 KHz Ultrasound, the TRIG Pin of the
+        # Ultrasonic Sensor must be held HIGH for a minimum duration of 10µS. After this, the Ultrasonic Transmitter, will transmits a burst of 8-pulses
+        # of ultrasound at 40 KHz. Immediately, the control circuit in the sensor will change the state of the ECHO pin to HIGH. This pins stays HIGH
+        # until the ultrasound hits an object and returns to the Ultrasonic Receiver. (source: https://www.electronicshub.org/raspberry-pi-ultrasonic-sensor-interface-tutorial/)
         time.sleep(1E-6)
         GPIO.output(trigPin, 0)
+
         while GPIO.input(echoPin) == 0:
             pass
-        # Gives me the system time
+        # Gives us the system time (starting the stopwatch)
         echoStartTime = time.time()
         while GPIO.input(echoPin) == 1:
             pass
+        # Stopping the stopwatch
         echoStopTime = time.time()
         pingTravelTime = echoStopTime - echoStartTime
-        # Reporting the number in milli seconds
+
         distance = 34300 * (pingTravelTime/2)
-        # Rounding to one decimal point
+        # Rounding to two decimal point
         print(round(distance, 2), 'cm')
 
-        # sensor required a delay before sending and receiving the ping
+        # sensor requires a delay before sending and receiving the ping
         time.sleep(0.2)
 
 except KeyboardInterrupt():
